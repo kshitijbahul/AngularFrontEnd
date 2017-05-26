fcc1a0b64ef31f047027d0d70b64e7ddaac71adb
Kshitij Bahul <chunkks.3@gmail.com>
2017-05-26 17:34:49 +0530

    Integrating API to get Data

--- js/controller/landingPageController.js
+++ js/controller/landingPageController.js
@@ -6,14 +6,14 @@ var app = angular.module('invoiceManagerApp');
 //const colors = require('js/utils/color');
 app.controller('landingPageController',function($scope,$http,$interval,_){
     $scope.overview ={};
-    $scope.processingStates=[
-        {"label":'Pending',"value":1234,"theme":"dark-grey"},
+    /*$scope.processingStates=[
+        {"status":'Pending',"value":1234,"theme":"dark-grey"},
         {"label":'Pre-Processing',"value":1234,"theme":"dark-blue"},
         {"label":'Processing',"value":1234,"theme":"dark-orange"},
         {"label":'Merging',"value":1234,"theme":"dark-purple"},
         {"label":'Ready',"value":1234,"theme":"dark-green"},
         {"label":'Failed',"value":1234,"theme":"dark-red"}
-    ];
+    ];*/
     $scope.possibleOverviews = [
         {
             "name":"Today",
@@ -34,10 +34,29 @@ app.controller('landingPageController',function($scope,$http,$interval,_){
             "name":"This Month"
         },
     ]
+    $scope.getStates = function(){
+        /*$http.get('http://192.168.1.198:8888/getStatementCountByStatus').then(
+            function success(result){
+                $scope.processingStates = result.data;
+            },function error(error){
+                console.log('Error in response');
+        });*/
+        $http({
+            method : 'GET',
+            url: 'http://192.168.1.198:8888/getStatementCountByStatus',
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+        }).then(
+            function success(result){
+                $scope.processingStates = result.data;
+            },function error(error){
+                console.log('Error in response');
+        });
+    }
     $scope.init = function(){
   //      console.log('colors here are ::',colors);
         $scope.colors = ['#FF5500', '#A1D490','#00ADF9','#D792E8','#2B66B3'];
         $scope.name='Kshitij';
+        $scope.getStates();
         $scope.processingInvoicesChart ={
             data: [90,40,30,10,20],
             labels : ['Oslo','Bergen','Stavanger','Flekkefjord','Farsund'],
@@ -59,8 +78,9 @@ app.controller('landingPageController',function($scope,$http,$interval,_){
     $interval(function(){
         console.log('Came in here $interval',$scope.testValueChange);
         $scope.testValueChange = $scope.testValueChange+3;
-        _.forEach($scope.processingStates, function(eachState){
-            return eachState.value=eachState.value+3;
-        });
+        // _.forEach($scope.processingStates, function(eachState){
+        //     return eachState.value=eachState.value+3;
+        // });
+        $scope.getStates();
     },10000)
 });
