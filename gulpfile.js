e775580a2a5efed199ed890d148736061b18eeba
KshitijBahul <chunks.3@gmail.com>
2017-05-27 10:51:45 +0530

    atleast it builds with browserify

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,9 @@ var connect = require('gulp-connect');
 var path = require('path');
 var babelify = require('babelify');
 var browserify = require('browserify');
+var source =require ('vinyl-source-stream');
+var gulpif =require ('gulp-if');
+var ngAnnotate =require('browserify-ngannotate');
 
 gulp.task('sass', function(){
     console.log('In the css task',path.join(__dirname,'/dist/css'));
@@ -42,10 +45,50 @@ gulp.task('combineAppJs',function(){
 });
 gulp.task('browserify',function(){
     console.log('In the browserify task',path.join(__dirname,'/dist/js'));
-    return gulp.src(path.join(__dirname,'/js/invoiceManager.js'))
+    /*return gulp.src(path.join(__dirname,'/js/invoiceManager.js'))
     .pipe(browserify().transform("babelify", {extensions: [".babel"]}))
     .pipe(concat('app.js'))
-    .pipe(gulp.dest(path.join(__dirname,'/dist/js')));
+    .pipe(gulp.dest(path.join(__dirname,'/dist/js')));*/
+    const shouldCreateSourcemap = false;
+    let bundler = browserify({
+        entries: [path.join(__dirname,'/js/invoiceManager.js')],
+        debug: true,
+        cache: {},
+        packageCache: {},
+        fullPaths: false
+    });
+    const transforms = [
+        { name: babelify, options: {} },
+        //{ name: debowerify, options: {} },
+        //{ name: ngAnnotate, options: {} },
+        //{ name: 'brfs', options: {} },
+        //{ name: bulkify, options: {} },
+        //{ name: envify, options: {} }
+    ];
+    transforms.forEach(function(transform) {
+        bundler.transform(transform.name, transform.options);
+    });
+
+    function rebundle() {
+        //bundleLogger.start();
+
+        const stream = bundler.bundle();
+        const sourceMapLocation = false;
+
+        return stream
+        // .on('error', handleErrors)
+        // .on('end', bundleLogger.end)
+        .pipe(source('invoiceManager.js'))
+        //.pipe(gulpif(shouldCreateSourcemap, buffer()))
+        //.pipe(gulpif(shouldCreateSourcemap, sourcemaps.init({ loadMaps: true })))
+        /*.pipe(gulpif(false, streamify(uglify({
+            compress: { drop_console: true } // eslint-disable-line camelcase
+        }))))*/
+        //.pipe(gulpif(shouldCreateSourcemap, sourcemaps.write(sourceMapLocation)))
+        .pipe(gulp.dest(path.join(__dirname,'/dist/js')));
+        //.pipe(browserSync.stream());
+    };
+    rebundle();
 });
 gulp.task('connect',function(){
     connect.server({
@@ -55,7 +98,7 @@ gulp.task('connect',function(){
 });
 gulp.task('watch',function(){
     gulp.watch(path.join(__dirname,'/scss/**/*.scss'),['sass']);
-    gulp.watch([path.join(__dirname,'/js/**/*.js'),path.join(__dirname,'/components/**/*.js')],['cleanJsFiles','combineAppJs']);
+    gulp.watch([path.join(__dirname,'/js/**/*.js'),path.join(__dirname,'/components/**/*.js')],['cleanJsFiles','browserify']);
 });
-//gulp.task('default',['testGulp','cleanAppFiles','sass','combineAppJs','connect','watch']);
-gulp.task('default',['browserify'])
+gulp.task('default',['testGulp','cleanAppFiles','sass','browserify','connect','watch']);
+//gulp.task('default',['browserify'])
